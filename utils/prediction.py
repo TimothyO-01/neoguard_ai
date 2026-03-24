88c48ff4cf6a1878ee460040330f67c08e6a44a1
Ojedokun Timothy Ayobami <ojedokuntimothy05@gmail.com>
2026-03-24 02:28:54 +0100

    Model predictions function
    Refactor risk prediction logic for clarity and precision.

--- utils/prediction.py
+++ utils/prediction.py
@@ -1,7 +1,12 @@
+
 import numpy as np
 
 def predict_risk(model, df):
-    """Predicts neonatal risk and returns class and probability."""
-    pred_prob = model.predict_proba(df)[:,1]
-    pred_class = np.where(pred_prob > 0.5, "High Risk", "Low/Moderate Risk")[0]
-    return pred_class, pred_prob[0]
+    pred_prob = model.predict_proba(df)[:, 1]
+    
+    if pred_prob[0] >= 0.5:
+        pred_class = "High Risk"
+    else:
+        pred_class = "Low/Moderate Risk"
+        
+    return pred_class, float(pred_prob[0])
