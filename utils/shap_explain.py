5bea3035bd5954a32470a9ec751546383642eb20
Ojedokun Timothy Ayobami <ojedokuntimothy05@gmail.com>
2026-03-24 02:30:50 +0100

    Update shap_explain.py
    Refactor shap_summary_plot and shap_single_plot functions to use input features directly. Update function parameters for consistency.

--- utils/shap_explain.py
+++ utils/shap_explain.py
@@ -1,14 +1,27 @@
+
 import shap
-import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib.pyplot as plt
 
-def shap_summary_plot(model, features):
+def shap_summary_plot(model, X):
     explainer = shap.TreeExplainer(model)
-    # Dummy data for summary (use model training data ideally)
-    shap_values = explainer.shap_values(model._Booster)
-    st.pyplot(shap.summary_plot(shap_values, features, show=False))
+    shap_values = explainer.shap_values(X)
 
-def shap_single_plot(model, input_df, features):
+    if isinstance(shap_values, list):
+        shap_values = shap_values[1]
+
+    fig, ax = plt.subplots()
+    shap.summary_plot(shap_values, X, show=False)
+    st.pyplot(fig)
+
+
+def shap_single_plot(model, X, feature_names):
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(input_df)
-    st.pyplot(shap.force_plot(explainer.expected_value[1], shap_values[1], input_df, matplotlib=True))
+    shap_values = explainer.shap_values(X)
+
+    if isinstance(shap_values, list):
+        shap_values = shap_values[1]
+
+    fig, ax = plt.subplots()
+    shap.bar_plot(shap_values[0], feature_names=feature_names)
+    st.pyplot(fig)
